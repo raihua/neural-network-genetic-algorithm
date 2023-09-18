01af28e07ff2975de94c467527b490d5dbeaed9b
raihua <raihua.dev@gmail.com>
2023-09-18 14:18:37 +0930

    Fix: to_categorial not working

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ Based on:
 from keras.datasets import mnist, cifar10
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.utils.np_utils import to_categorical
+from keras.utils import to_categorical
 from keras.callbacks import EarlyStopping
 
 # Helper: Early stopping.
