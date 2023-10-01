2defee0e76790761b19d0d97f1629f9d3c1920f7
raihua <raihua.dev@gmail.com>
2023-10-01 19:17:59 +1030

    Add: test mutate

--- test_optimizer.py
+++ test_optimizer.py
@@ -14,6 +14,9 @@ class TestOptimiser(unittest.TestCase):
         self.optimizer = Optimizer(self.nn_param_choices)
         # self.network = Network(self.nn_param_choices)
 
+        self.network1 = {'nb_neurons': 1024, 'nb_layers': 1, 'activation': 'relu', 'optimizer': 'sgd'}
+        self.network2 = {'nb_neurons': 64, 'nb_layers': 3, 'activation': 'tanh', 'optimizer': 'adagrad'}
+
     def test_create_population(self):
         networkList = self.optimizer.create_population(1)
 
@@ -35,17 +38,20 @@ class TestOptimiser(unittest.TestCase):
         self.assertEqual(self.optimizer.grade(networkList), manualMeanFitnessPopulation, "Average does not match.")
 
     def test_breed(self):
-        network1 = {'nb_neurons': 1024, 'nb_layers': 1, 'activation': 'relu', 'optimizer': 'sgd'}
-        network2 = {'nb_neurons': 64, 'nb_layers': 3, 'activation': 'tanh', 'optimizer': 'adagrad'}
-        offspring = self.optimizer.breed(network1, network2)
+        
+        offspring = self.optimizer.breed(self.network1, self.network2)
 
         self.assertEqual(len(offspring), 2, "Not a list of 2 objects.")
         self.assertEqual(type(offspring[0]), Network, "Offspring not a network.")
-        self.assertNotEqual(offspring[0], network1, "Offspring still the same as parent.")
+        self.assertNotEqual(offspring[0], self.network1, "Offspring still the same as parent.")
         
     def test_mutate(self):
-        
+        self.optimizer.mutate_chance = 0.99
+        network1original = {'nb_neurons': 1024, 'nb_layers': 1, 'activation': 'relu', 'optimizer': 'sgd'}
+        mutatedNetwork = self.optimizer.mutate(self.network1)
+        self.assertNotEqual(mutatedNetwork.network.items(), network1original.items(), "Failed to mutate")
 
+    
 
 if __name__ == '__main__':
     unittest.main()
