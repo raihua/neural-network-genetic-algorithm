ae3bd2979a257c15ddc7ad8519cf5cdafc3a781b
raihua <raihua.dev@gmail.com>
2023-10-01 18:38:58 +1030

    Add: unittest optimizer

---
+++ test_optimizer.py
@@ -0,0 +1,51 @@
+import unittest
+from optimizer import Optimizer
+from network import Network
+
+class TestOptimiser(unittest.TestCase):
+    def setUp(self):
+        self.nn_param_choices = {
+        'nb_neurons': [64, 128, 256, 512, 768, 1024],
+        'nb_layers': [1, 2, 3, 4],
+        'activation': ['relu', 'elu', 'tanh', 'sigmoid'],
+        'optimizer': ['rmsprop', 'adam', 'sgd', 'adagrad','adadelta', 'adamax', 'nadam'],
+        }
+
+        self.optimizer = Optimizer(self.nn_param_choices)
+        # self.network = Network(self.nn_param_choices)
+
+    def test_create_population(self):
+        networkList = self.optimizer.create_population(1)
+
+        #check type
+        self.assertIsInstance(networkList[0], Network, "Population not created.")
+
+        #check keys
+        self.assertEqual(networkList[0].network.keys(), self.nn_param_choices.keys(), "Keys not equal.")
+
+    def test_fitness(self):
+        networkList = self.optimizer.create_population(1)
+        networkList[0].accuracy = 20
+        self.assertEqual(self.optimizer.fitness(networkList[0]), 20, "Fitness doesn't match.")
+
+    def test_grade(self):
+        networkList = self.optimizer.create_population(2)
+        manualMeanFitnessPopulation = (networkList[0].accuracy + networkList[1].accuracy) / 2
+
+        self.assertEqual(self.optimizer.grade(networkList), manualMeanFitnessPopulation, "Average does not match.")
+
+    def test_breed(self):
+        network1 = {'nb_neurons': 1024, 'nb_layers': 1, 'activation': 'relu', 'optimizer': 'sgd'}
+        network2 = {'nb_neurons': 64, 'nb_layers': 3, 'activation': 'tanh', 'optimizer': 'adagrad'}
+        offspring = self.optimizer.breed(network1, network2)
+
+        self.assertEqual(len(offspring), 2, "Not a list of 2 objects.")
+        self.assertEqual(type(offspring[0]), Network, "Offspring not a network.")
+        self.assertNotEqual(offspring[0], network1, "Offspring still the same as parent.")
+        
+    def test_mutate(self):
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
